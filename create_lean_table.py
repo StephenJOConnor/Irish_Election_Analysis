d150d6b4f775b34830ec718f9e9fd5146b4b4792
Stephen O'Connor <sjp.oconnor@gmail.com>
2019-09-30 14:51:53 -0700

    recent changes

--- create_lean_table.py
+++ create_lean_table.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import numpy as np
 import ast
-import matplotlib.pyplot as plt
+from sqlite3 import connect
 
 # In[4]:
 
@@ -88,29 +88,58 @@ for i in range(1, max(dail['rounds'])):
     dail = pd.merge(dail, vote_out_ag, how = 'left', on = 'race')
     dail['agg_vote_out_' + str(i)] = dail['agg_vote_out_' + str(i)].fillna(0).astype(int)
 
+#%%
+
+#if elemination_rd is 0, make elemination round the last round
+dail['elimination_rd'] = dail.apply(lambda x: len(x['round_totals']) if x['elimination_rd']==0 else x['elimination_rd'], axis=1)
+
+#%%
+
+dail_choice = pd.read_sql_query("select race, count(party) from (select distinct race, party " + \
+                                "from dail group by race, party having elimination_rd > 1) group by race", con)
+
 
 # In[25]:
+
+con = connect(":memory:")
+
+dail.drop(['transfers','round_totals'], axis=1).to_sql(name='dail', con=con)
 
 #create dataframe to record all transfers between individual parties and their pct share of all transferred votes
 for i in range(1, max(dail['rounds'])):
     
+    #find number of parties left in the race per round
+    rnd_choice = pd.read_sql_query("select race, count(party) as rnd_" + str(i) + " from (select distinct race, party " + \
+                                "from dail group by race, party having elimination_rd > " + str(i) + ") group by race", con)
+    
     if i==1:
+        
+        #create the rnd_choices df
+        rnd_choices = rnd_choice
+        
         #find aggregate inward vote by party/election/race for round 1
-        transfers = pd.DataFrame(dail.groupby(['elec_num', 'year', 'party', 'single_party_transfer_' + str(i),  'race', 'agg_vote_out_' + str(i)], \
-                                            as_index=False)['inward_vote_' + str(i)].sum())
+        const_data = pd.DataFrame(dail.groupby(['elec_num', 'year', 'party', 'single_party_transfer_' + str(i),  'race', 'constit', 'agg_vote_out_' + str(i)], \
+                                            as_index=False)['inward_vote_' + str(i)].sum())        
         
         #find the aggregate outword vote by party/election
-        transfers = pd.DataFrame(transfers.groupby(['elec_num', 'year', 'party', 'single_party_transfer_' + str(i)], \
+        transfers = pd.DataFrame(const_data.groupby(['elec_num', 'year', 'party', 'single_party_transfer_' + str(i)], \
                                             as_index=False)['inward_vote_' + str(i), 'agg_vote_out_' + str(i)].sum())
         
     else:
         
+        #update the rnd_choices df
+        rnd_choices = pd.merge(rnd_choices, rnd_choice, how = "left", left_on = ['race'], right_on = ['race'])
+        
         #find aggregate inward vote by party/election/race for round i
-        transferred = pd.DataFrame(dail.groupby(['elec_num', 'party', 'year', 'single_party_transfer_' + str(i),  'race', 'agg_vote_out_' + str(i)], \
+        const_data_2 = pd.DataFrame(dail.groupby(['elec_num', 'party', 'year', 'single_party_transfer_' + str(i),  'race', 'constit', 'agg_vote_out_' + str(i)], \
                                             as_index=False)['inward_vote_' + str(i)].sum())
         
+        #merge constituent-level data
+        const_data = pd.merge(const_data, const_data_2, how = 'outer', left_on = ['elec_num', 'year', 'party', 'single_party_transfer_1', 'race', 'constit'], \
+                                                                         right_on = ['elec_num', 'year', 'party', 'single_party_transfer_' + str(i), 'race', 'constit'])
+        
         #find aggregate outward vote by party/election round i
-        transferred = pd.DataFrame(transferred.groupby(['elec_num', 'party', 'year','single_party_transfer_' + str(i)], \
+        transferred = pd.DataFrame(const_data_2.groupby(['elec_num', 'party', 'year','single_party_transfer_' + str(i)], \
                                             as_index=False)['inward_vote_' + str(i), 'agg_vote_out_' + str(i)].sum())
         
         transfers = pd.merge(transfers, transferred, how = 'outer', left_on = ['elec_num', 'year', 'party', 'single_party_transfer_1'], \
@@ -119,43 +148,25 @@ for i in range(1, max(dail['rounds'])):
         transfers['single_party_transfer_1'] = np.where(transfers['single_party_transfer_1'].isnull(), \
            transfers['single_party_transfer_' + str(i)], transfers['single_party_transfer_1'])
         
+        const_data['single_party_transfer_1'] = np.where(const_data['single_party_transfer_1'].isnull(), \
+           const_data['single_party_transfer_' + str(i)], const_data['single_party_transfer_1'])
+        
         
         #drop transfer party name
         transfers = transfers.drop('single_party_transfer_' + str(i), axis=1)
+        const_data = const_data.drop('single_party_transfer_' + str(i), axis=1)
+        
     
+const_data = const_data.rename(columns = {"single_party_transfer_1": "transfer_party"})
 transfers = transfers.rename(columns = {"single_party_transfer_1": "transfer_party"})
 
 #sort transfers by elec_num
+const_data = const_data.sort_values(by=['elec_num'])
 transfers = transfers.sort_values(by=['elec_num'])
 
 #drop feb 1982 election (it's missing transfers)
 transfers = transfers[transfers['elec_num']>0]
-
-# In[25]:
-
-#get rid of null values and calculate pct_trans for each round
-for i in range(1, max(dail['rounds'])):
-    #fill nans w/ 0
-    transfers['agg_vote_out_' + str(i)] = transfers['agg_vote_out_' + str(i)].fillna(0).astype(int)
-    transfers['inward_vote_' + str(i)] = transfers['inward_vote_' + str(i)].fillna(0).astype(int)
-    
-    if i>1:
-        #calculate cumulative transfers between individual parties
-        transfers['cum_trans_out_' + str(i)] = transfers['agg_vote_out_' + str(i)] + transfers['cum_trans_out_' + str(i-1)]
-        transfers['cum_trans_in_' + str(i)] = transfers['inward_vote_' + str(i)] + transfers['cum_trans_in_' + str(i-1)]     
-        
-    else:
-        transfers['cum_trans_out_' + str(i)] = transfers['agg_vote_out_'  + str(i)]
-        transfers['cum_trans_in_' + str(i)] = transfers['inward_vote_'  + str(i)]
-    
-    #calculate pct transfers per round
-    transfers['rnd_pct_trans_' + str(i)] = transfers['inward_vote_'  + str(i)]/transfers['agg_vote_out_'  + str(i)]
-    transfers['cum_pct_trans_' + str(i)] = transfers['cum_trans_in_' + str(i)]/transfers['cum_trans_out_' + str(i)]
-    
-    #fill nas when denominator is zero
-    transfers['rnd_pct_trans_' + str(i)] = transfers['rnd_pct_trans_' + str(i)].fillna(0)
-    transfers['cum_pct_trans_' + str(i)] = transfers['cum_pct_trans_' + str(i)].fillna(0)
-
+const_data = const_data[const_data['elec_num']>0]
 
 # In[27]:
 
@@ -176,3 +187,66 @@ first_pref_count['first_pref_pct'] = first_pref_count['inward_vote_0']/first_pre
 
 transfers.to_pickle("./transfers_82_16.pkl")
 first_pref_count.to_pickle("./first_prefs.pkl")
+const_data.to_pickle("./const_data.pkl")
+rnd_choices.to_pickle("./rnd_choices.pkl")
+
+# In[25]:
+
+def analysis_tables(transfers, const_data, rnd_choices, min_choice=0):
+
+	
+	transfers = pd.merge(transfers, rnd_choices, how = "left", on = ['race'])
+	const_data = pd.merge(const_data, rnd_choices, how = "left", on = ['race'])
+	
+	
+	
+	#get rid of null values and calculate pct_trans for each round
+	for i in range(1, 18):
+	    #fill nans w/ 0
+	    transfers['agg_vote_out_' + str(i)] = transfers['agg_vote_out_' + str(i)].fillna(0).astype(int)
+	    transfers['inward_vote_' + str(i)] = transfers['inward_vote_' + str(i)].fillna(0).astype(int)
+		
+		#void aggregate transfers where min number of party choices don't exist 
+		transfers['agg_vote_out_' + str(i)] = transfers.apply(lambda x: x['agg_vote_out_' + str(i)] \
+			if x['rnd_' + str(i)] > min_choice else 0)
+		transfers['inward_vote_' + str(i)] = transfers.apply(lambda x: x['inward_vote_' + str(i)] \
+			if x['rnd_' + str(i)] > min_choice else 0)
+	    
+		#fill nans w/ 0
+	    const_data['agg_vote_out_' + str(i)] = const_data['agg_vote_out_' + str(i)].fillna(0).astype(int)
+	    const_data['inward_vote_' + str(i)] = const_data['inward_vote_' + str(i)].fillna(0).astype(int)
+		
+		#void constituency transfers where min number of party choices don't exist 
+		
+		
+		
+	    
+	    if i>1:
+	        #calculate cumulative transfers between individual parties
+	        transfers['cum_trans_out_' + str(i)] = transfers['agg_vote_out_' + str(i)] + transfers['cum_trans_out_' + str(i-1)]
+	        transfers['cum_trans_in_' + str(i)] = transfers['inward_vote_' + str(i)] + transfers['cum_trans_in_' + str(i-1)]
+	
+	        const_data['cum_trans_out_' + str(i)] = const_data['agg_vote_out_' + str(i)] + const_data['cum_trans_out_' + str(i-1)]
+	        const_data['cum_trans_in_' + str(i)] = const_data['inward_vote_' + str(i)] + const_data['cum_trans_in_' + str(i-1)]                   
+	    else:
+	        transfers['cum_trans_out_' + str(i)] = transfers['agg_vote_out_'  + str(i)]
+	        transfers['cum_trans_in_' + str(i)] = transfers['inward_vote_'  + str(i)]
+	        
+	        const_data['cum_trans_out_' + str(i)] = const_data['agg_vote_out_'  + str(i)]
+	        const_data['cum_trans_in_' + str(i)] = const_data['inward_vote_'  + str(i)]
+	    
+	    #calculate pct transfers per round
+	    transfers['rnd_pct_trans_' + str(i)] = transfers['inward_vote_'  + str(i)]/transfers['agg_vote_out_'  + str(i)]
+	    transfers['cum_pct_trans_' + str(i)] = transfers['cum_trans_in_' + str(i)]/transfers['cum_trans_out_' + str(i)]
+	    
+	    const_data['rnd_pct_trans_' + str(i)] = const_data['inward_vote_'  + str(i)]/const_data['agg_vote_out_'  + str(i)]
+	    const_data['cum_pct_trans_' + str(i)] = const_data['cum_trans_in_' + str(i)]/const_data['cum_trans_out_' + str(i)]
+	    
+	    #fill nas when denominator is zero
+	    transfers['rnd_pct_trans_' + str(i)] = transfers['rnd_pct_trans_' + str(i)].fillna(0)
+	    transfers['cum_pct_trans_' + str(i)] = transfers['cum_pct_trans_' + str(i)].fillna(0)
+	    
+	    const_data['rnd_pct_trans_' + str(i)] = const_data['rnd_pct_trans_' + str(i)].fillna(0)
+	    const_data['cum_pct_trans_' + str(i)] = const_data['cum_pct_trans_' + str(i)].fillna(0)
+		
+		return(transfers, const_data)
---
+++ maps.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 09:42:48 2019
+
+@author: sjpoc
+"""
+
+import geopandas as gpd
+
+#%%
+
